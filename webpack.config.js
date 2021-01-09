502c75fae6e31df16d4b47722bb73f7e08b958ac
metalbluster1 <vijaykumar.wolverine@gmail.com>
2021-01-09 16:38:54 +0530

    Completed desktop and mobile header style

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const postCSSPlugins = [
     require('postcss-simple-vars'),
     require('postcss-mixins'),
     require('postcss-nested'),
+    require('postcss-hexrgba'),
     require('autoprefixer')
 ]
 
