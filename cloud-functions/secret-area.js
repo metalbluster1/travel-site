7105c1ef001eeb4732b7b56d1b53bec7e1e64289
metalbluster1 <vijaykumar.wolverine@gmail.com>
2021-01-25 20:37:56 +0530

    Done setting cloud-function password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    const secretContent = `
+    <h3>Welcome to secret area</h3>
+    <p>Challapalli is located in <strong>Andhra Pradesh</strong></p>
+    `
+
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    }
+    else {
+        body = {}
+    }
+
+    if (body.password == "Challapalli") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+
+
 }
