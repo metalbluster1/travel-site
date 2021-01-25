090b182a497b203883b604ffc29e0f98d60be6ca
metalbluster1 <vijaykumar.wolverine@gmail.com>
2021-01-25 23:18:41 +0530

    Add client area section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,9 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/sticky-header'
+import ClientArean from './modules/Client_Area'
 
+new ClientArean()
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-items"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
